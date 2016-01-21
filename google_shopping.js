e1540cecd7ff02a533000f08a0b21d551268ce14
navyayvan <navyayvan@gmail.com>
2016-01-20 23:14:58 -0800

    incomplete google_shopping.js

--- google_shopping.js
+++ google_shopping.js
@@ -2,4 +2,52 @@ var data = require("./products.json")
 
 // Write your solutions below
 
-console.log(data["items"]);
+for (var kindItem in items) {
+  if (kindItem === "shopping#product") {
+    console.log(kindItem)
+  }
+
+  /* also stored in itemsPerPage key, value of 25 */
+
+}
+
+for (var availability in inventories) {
+	if (availability === "backorder") {
+		console.log(availability);
+	}
+	else {
+		return null;
+	}
+}
+
+for (var imageLink in products) {
+	if ( images[0] && images(1) === true ) {
+		console.log(images)
+	}
+	else {
+		return null
+	}
+}
+
+for 
+
+for (var productAuthor in author) {
+	if (name === "eBay" && brand = "Canon") {
+		console.log (productAuthor);
+	}
+	else {
+		return null 
+	}
+}
+var condensedArray = function(brand, name, price) {
+
+	var brand = function (
+			for (i = 0, )
+		)
+
+
+}
+
+}
+
+console.log(data["items"]); 
